0f2c42c72f3a36bf9cd83e6675a2fd3f63471382
josh <josh.kay@outlook.com>
2015-03-03 23:36:25 -0800

    default pages placed

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -12,5 +12,25 @@ angular.module('kelsikaymakeupApp')
         url: '/',
         templateUrl: 'app/partials/about.html',
         controller: 'MainCtrl'
+      })
+      .state('services', {
+        url: '/',
+        templateUrl: 'app/partials/services.html',
+        controller: 'MainCtrl'
+      })
+      .state('gallery', {
+        url: '/',
+        templateUrl: 'app/partials/Gallery.html',
+        controller: 'MainCtrl'
+      })
+      .state('testimonials', {
+        url: '/',
+        templateUrl: 'app/partials/testimonials.html',
+        controller: 'MainCtrl'
+      })
+      .state('contact', {
+        url: '/',
+        templateUrl: 'app/partials/contact.html',
+        controller: 'MainCtrl'
       });
   });
