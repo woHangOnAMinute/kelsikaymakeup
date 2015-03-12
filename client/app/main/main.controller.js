050189647ae2ea6038a3e81f732640146e479653
WoHangOnAMinute <josh.kay@outlook.com>
2015-03-12 23:51:57 +1100

    fixed styling pushed to live and removed fake testimonials

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,9 +5,9 @@ angular.module('kelsikaymakeupApp')
     $scope.message = 'OH MY GOODNESS';
 
     $scope.testimonials = [];
-    $scope.testimonials.push({'quote':'"suscipit enim eleifend, imperdiet posuere ante."', 'name':'- Josh Kay'});
-    $scope.testimonials.push({'quote':'"Lorem ipsum dolor sit amet, consectetur "', 'name':'- Leah White'});
-    $scope.testimonials.push({'quote':'"suscipit enim eleifend, imperdiet posuere ante."', 'name':'- Jonah Lomu'});
+    $scope.testimonials.push({'quote':'"Kelsi did an absolutely fantastic job of my hair and make up for a red carpet event I attended. I usually struggle getting the right colour for my skin tone and other make up artists have made me look very orange. Kelsi kept my make up nice and natural with elegant touches to dress it up for the occasion. She has the best selection of lipstick colours too! I highly recommend her services and had many compliments on the night - stunning was the word I heard most!"', 'name':'- Emma'});
+    // $scope.testimonials.push({'quote':'"Lorem ipsum dolor sit amet, consectetur "', 'name':'- Leah White'});
+    // $scope.testimonials.push({'quote':'"suscipit enim eleifend, imperdiet posuere ante."', 'name':'- Jonah Lomu'});
 
     $scope.myInterval = 3000;
     var slides = $scope.slides = [];
